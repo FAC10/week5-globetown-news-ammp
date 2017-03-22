a2c18dfade24625808fcd526722ad7b5f3f25955
PiotrBerebecki <petebere@gmail.com>
2017-03-22 14:02:56 +0000

    Setup assets and fake env
    relates #7 #2

--- src/handlers.js
+++ src/handlers.js
@@ -1,22 +1,39 @@
 const fs = require('fs');
 const path = require('path');
+const urlModule = require('url');
 
 
-const env = require ('env2')('../.env');
+require('env2')('.env');
 console.log('newsKey', process.env.newsKey);
 console.log('tflKey', process.env.tflKey);
 
 
 const handlers = {};
 
-handlers.serveLanding = (req, res) => {
-  const readStream = fs.createReadStream(path.join(__dirname, '..', 'public', 'index.html'));
 
-  readStream.on('open', () => {
-    res.writeHead(200, {'Content-Type':'text/html'});
-    readStream.pipe(res);
-  });
-  
+handlers.serveLanding = (req, res) => {
+  res.writeHead(200, {'Content-Type':'text/html'});
+  const readStream = fs.createReadStream(path.join(__dirname, '..', 'public', 'index.html'));
+  readStream.pipe(res);
 };
+
+
+function getContentType(url) {
+  const extension = path.extname(url);
+  const extensionType = {
+    '.css': 'text/css',
+    '.js': 'application/javascript',
+    '.ico': 'image/x-icon'
+  };
+  return extensionType[extension];
+}
+
+
+handlers.serveAssets = (req, res) => {
+  res.writeHead(200, {'Content-Type': getContentType(req.url)});
+  const readStream = fs.createReadStream(path.join(__dirname, '..', 'public', req.url));
+  readStream.pipe(res);
+};
+
 
 module.exports = handlers;
