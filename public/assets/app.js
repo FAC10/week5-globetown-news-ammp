1e6705954e340afa8e121343de13b00ff66c4b3f
PiotrBerebecki <petebere@gmail.com>
2017-03-22 16:59:07 +0000

    Add basic layout
    Relates #11

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,1 +1,28 @@
-console.log("hello");
+// *************************************************
+// APP
+// *************************************************
+fetch('GET', 'http://localhost:4000/news', function(err, res) {
+  console.log(res);
+});
+
+
+// *************************************************
+// FETCH
+// *************************************************
+function fetch(method, url, handleResponseCallback) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var jsonObj = JSON.parse(xhr.responseText);
+      handleResponseCallback(null, jsonObj);
+    }
+  };
+
+  xhr.onerror = function() {
+    handleResponseCallback('Api response error');
+  };
+
+  xhr.open(method, url, true);
+  xhr.send();
+}
