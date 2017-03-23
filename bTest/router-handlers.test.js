78fb1d4587565dcdf6c631b0ca7d43fe82ebdd74
PiotrBerebecki <petebere@gmail.com>
2017-03-23 14:30:39 +0000

    Add backend tests
    relates #24

--- bTest/router-handlers.test.js
+++ bTest/router-handlers.test.js
@@ -1,7 +1,7 @@
 const test = require('tape');
 const shot = require('shot');
 const router = require('../src/router.js');
-// const  = require('../src/.js');
+const handlers = require('../src/handlers.js');
 
 
 test('Initialise tests', (t) => {
@@ -9,25 +9,40 @@ test('Initialise tests', (t) => {
   t.end();
 });
 
+
 const fake = [
-  {method: 'GET',url:'/',expectedContentType:'text/html',expectedStatusCode: 200},
-  {method: 'GET',url:'/news',expectedContentType:'application/json',expectedStatusCode: 200},
-  {method: 'GET',url:'/elephant',expectedContentType:'text/html',expectedStatusCode: 404}
+  {method: 'GET', url:'/', expectedContentType:'text/html', expectedStatusCode: 200},
+  {method: 'GET', url:'/news', expectedContentType:'application/json', expectedStatusCode: 200},
+  {method: 'GET', url:'/elephant', expectedContentType:'text/html', expectedStatusCode: 404}
 ];
 
 
 fake.forEach(route => {
-  const {method,url,expectedStatusCode,expectedContentType} = route;
-  test('Test and handlers', (t) => {
-    shot.inject(router, { method: method , url: url } , (res) => {
-      t.equal(res.statusCode, expectedStatusCode,'Should return correct Status Code');
-      t.equal(res.headers['content-type'],expectedContentType,'Should return correct content type');
+  const {method, url, expectedStatusCode, expectedContentType} = route;
+  test(`Test '${url}' route`, (t) => {
+    shot.inject(router, { method: method, url: url } , (res) => {
+      t.equal(res.statusCode, expectedStatusCode, `should return status code of ${expectedStatusCode}`);
+      t.equal(res.headers['content-type'], expectedContentType, `should return content type of ${expectedContentType}`);
       t.end();
     });
   });
 });
 
 
+test('Test getContentType function', (t) => {
+  const fakeContentTypes = [
+    {url: '/assets/app.js', expectedContentType: 'application/javascript'},
+    {url: '/assets/style.css', expectedContentType: 'text/css'},
+    {url: '/assets/favicon.ico', expectedContentType: 'image/x-icon'}
+  ];
+
+  const randomIndex = Math.floor(Math.random() * fakeContentTypes.length);
+  const actualContentType = handlers.getContentType(fakeContentTypes[randomIndex].url);
+  const expectedContentType = fakeContentTypes[randomIndex].expectedContentType;
+
+  t.equal(actualContentType, expectedContentType, `should return content type of ${expectedContentType} for url of ${fakeContentTypes[randomIndex].url}`);
+  t.end();
+});
 
 
 // test('Test serve news handler', (t) => {
